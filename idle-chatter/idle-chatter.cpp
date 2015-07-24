61bdec5a3944e8086a8f8ba11a8ab47c72405181
SonicFreak94 <sonicfreak94@hotmail.com>
2015-07-24 17:10:59 -0600

    Updated mod loader and removed Controllers in favor of ControllersRaw

--- idle-chatter/idle-chatter.cpp
+++ idle-chatter/idle-chatter.cpp
@@ -1,13 +1,11 @@
 #include <SADXModLoader.h>
 
-DataArray(ControllerData, Controllers, 0x03B0E9C8, 8);
-
 extern "C"
 {
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
 	__declspec(dllexport) void OnFrame()
 	{
-		if (Controllers[0].PressedButtons & Buttons_Z)
+		if (ControllersRaw[0].PressedButtons & Buttons_Z)
 		{
 			CharObj2* player = GetCharObj2(0);
 			if (player != nullptr)
